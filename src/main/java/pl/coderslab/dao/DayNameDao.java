737b969c7fc17097340a793a9dabf98a1ef234b0
Marcin <marcin3216@gmail.com>
2019-04-15 21:20:26 +0200

    DayNameDao

---
+++ src/main/java/pl/coderslab/dao/DayNameDao.java
@@ -0,0 +1,38 @@
+package pl.coderslab.dao;
+
+import pl.coderslab.model.DayName;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayNameDao {
+
+    // Zapytania SQL
+    private static final String FIND_ALL_DAY_NAME_QUERY = "SELECT * FROM day_name";
+
+    public List<DayName> findAll() {
+        List<DayName> dayNamelist = new ArrayList<>();
+
+        try(Connection connection = DbUtil.getConnection();
+            PreparedStatement statement = connection.prepareStatement(FIND_ALL_DAY_NAME_QUERY);
+            ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                DayName dayNameToAdd = new DayName();
+                dayNameToAdd.setId(resultSet.getInt("id"));
+                dayNameToAdd.setName(resultSet.getString("name"));
+                dayNameToAdd.setDisplayOrder(resultSet.getInt("display_order"));
+                dayNamelist.add(dayNameToAdd);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dayNamelist;
+    }
+}
